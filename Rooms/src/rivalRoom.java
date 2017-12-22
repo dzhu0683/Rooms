430a41f3c0e70229abc2cf5abfd378214d87c2db
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-12-22 09:26:20 -0500

    rivals

--- Rooms/src/rivalRoom.java
+++ Rooms/src/rivalRoom.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class rivalRoom extends Room
 {
 	public rivalRoom(int x, int y) 
@@ -16,11 +16,26 @@ public class rivalRoom extends Room
 		occupant = x;
 		x.setxLoc(this.xLoc);
 		x.setyLoc(this.yLoc);
-		System.out.println("You are challenged by your rival Shiftry to battle.");
+		System.out.println("You discover your rival Shiftry doing something suspicious");
+		System.out.println("Before you can get a better look at what he's doing, Shiftry discovers you and attacks you!");
 		
 		while (rivalBattle)
 		{
+			Scanner sc = new Scanner(System.in);
 			System.out.println("Shiftry is charging his Hyper Beam");
+			System.out.println("What do you do?");
+			System.out.println("1. Attack!   2. Block!");
+			System.out.println("3. Run!");
+			int A = sc.nextInt();
+			switch (A) 
+			{
+				case 1:
+					System.out.println("You use Flamethrower!");
+					System.out.println("Your rival Shiftry has fainted!");
+					System.out.println("You have fulfilled the request!");
+					System.out.println("You return to town triumphantly");
+					rivalBattle = false;
+			}
 		}
 		
 	}	
